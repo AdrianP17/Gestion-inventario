daec714d02a15b6183dc022ac2ae27de0a974270
Adrian Palacios <78890986+AdrianP17@users.noreply.github.com>
2024-10-11 21:28:43 -0500

    MySQL Producto Repository, ProductoRoutes update

---
+++ backend/src/infrastructure/database/mysql/connection.ts
@@ -0,0 +1,11 @@
+import { createPool, Pool } from 'mysql2/promise';
+
+export const db: Pool = createPool({
+  host: 'localhost', // Cambia según la configuración de tu servidor MySQL
+  user: 'root',
+  password: '1234', // Cambia según tus credenciales
+  database: 'bbdd_inventario',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
