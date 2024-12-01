d9c90b9c7a648b027900206355cded24ef0574a9
S3RN4K <andrew.sernaq@gmail.com>
2024-12-01 14:30:17 -0500

    a

--- backend/src/domain/entities/DetalleSalida.ts
+++ backend/src/domain/entities/DetalleSalida.ts
@@ -1,16 +1,11 @@
 export class DetalleSalida {
     constructor(
-<<<<<<< Updated upstream
-      public id: number,
-      public salidaInventarioId: number,
-=======
       public ID: number,
       public Salida_InventarioID: number,
->>>>>>> Stashed changes
-      public productoId: number,
-      public cantidad: number,
-      public precioUnitario: number,
-      public subtotal: number,
+      public ProductoID: number,
+      public Cantidad: number,
+      public Precio_Unitario: number,
+      public Subtotal: number,
       public Estado: string
     ) {}
   }
--- backend/src/domain/entities/OrdenSalidaInventario.ts
+++ backend/src/domain/entities/OrdenSalidaInventario.ts
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream:backend/src/domain/entities/SalidaInventario.ts
-export class SalidaInventario {
-    constructor(
-      public id: number,
-      public fechaSalida: Date,
-      public area: string,
-      public estado: string,
-      public totalSalida: number
-=======
 export class OrdenSalidaInventario {
     constructor(
       public ID: number,
@@ -16,7 +7,6 @@ export class OrdenSalidaInventario {
       public Estado: string,
       public Total_Salida: number,
       public Observaciones: Text,
->>>>>>> Stashed changes:backend/src/domain/entities/OrdenSalidaInventario.ts
     ) {}
   }
   
